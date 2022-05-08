00e2770eb46d6412adf4bdf124be47ce329cb417
ngtruongthanh98 <ngtruongthanh98@gmail.com>
2022-05-08 08:51:05 +0700

    Import Tiktok logo.

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -2,14 +2,20 @@ import React from "react";
 import classNames from "classnames/bind";
 import styles from "./Header.module.scss";
 
+import images from "~/assets/images";
+
 const cx = classNames.bind(styles);
+
+console.log(images.logo.default);
 
 const Header = () => {
   return (
     <header className={cx("wrapper")}>
       <div className={cx("inner")}>
-        {/* Logo */}
-        {/* Search */}
+        {/* Logo Tiktok */}
+        <div className={cx("logo")}>
+          <img src={images.logo.default} alt="Tiktok" />
+        </div>
       </div>
     </header>
   );
