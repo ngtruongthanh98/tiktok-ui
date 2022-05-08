d18aeed53f9cb13b8d78df6ba534c0f2256569cb
ngtruongthanh98 <ngtruongthanh98@gmail.com>
2022-05-08 11:33:21 +0700

    Doing build UI header.

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import classNames from "classnames/bind";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import styles from "./Header.module.scss";
 
 import images from "~/assets/images";
@@ -13,9 +15,25 @@ const Header = () => {
     <header className={cx("wrapper")}>
       <div className={cx("inner")}>
         {/* Logo Tiktok */}
-        <div className={cx("logo")}>
+        <div>
           <img src={images.logo.default} alt="Tiktok" />
         </div>
+
+        {/* Search Input */}
+        <div className={cx("search")}>
+          <input type="text" placeholder="Search accounts and videos" spellCheck={false} />
+
+          <button className={cx("clear")}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
+
+          <button className={cx("search-btn")}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+
+        <div className={cx("actions")}></div>
       </div>
     </header>
   );
